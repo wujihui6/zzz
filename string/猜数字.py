3224f2cbfba4df00c85eb75cc7e32991eb3ba7b3
wujihui <1578347002@qq.com>
2022-12-04 19:36:02 +0800

    first commit

---
+++ string/猜数字.py
@@ -0,0 +1,15 @@
+from random import random
+# import random
+a = int(random()*100)
+# i = random.randint(1,100)   这两个的意思表达不一致
+while True:
+    j = int(input("请输入你要猜的数字"))
+    if j > a:
+        print("太大了")
+        continue
+    elif j < a:
+        print("太小了")
+        continue
+    else:
+        print("猜对了")
+        break
